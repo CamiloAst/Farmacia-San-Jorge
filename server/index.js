a115779e4a5159aa0182d5c5d02aefe6a528153b
Juan Camilo Astudillo <juanc.astudilloo@uqvirtual.edu.co>
2026-03-09 19:19:19 -0500

    despliegue

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ const inventoryRoutes = require('./routes/inventory');
 
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 app.use(cors());
 app.use(express.json());
 
